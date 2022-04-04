07ae5acc7790c73392100b646ad67e5b2e0b56b1
esra.alshafi@gmail.com <esra.alshafi@gmail.com>
2022-04-04 07:14:12 +0200

    add test url and json of test firebase

--- lib/core/values/urls.dart
+++ lib/core/values/urls.dart
@@ -1,5 +1,5 @@
-const baseURL = 'https://test.sabek.app/'; // cp -- test
-const apiDriverUrl = 'https://test.sabek.app/api/driver/';
-const apiBaseUrl = 'https://test.sabek.app/api/';
+const baseURL = 'https://tests.sabek.ly/'; // cp -- test
+const apiDriverUrl = 'https://tests.sabek.ly/api/driver/';
+const apiBaseUrl = 'https://tests.sabek.ly/api/';
 
 
